ab057d07642832be9c7e1b848bab27efd6124615
osnat <go.osnat@gmail.com>
2019-06-27 20:00:22 +0300

    not much has changed

--- webStoreFinal/Controllers/AccountController.cs
+++ webStoreFinal/Controllers/AccountController.cs
@@ -29,9 +29,10 @@ namespace webStoreFinal.Controllers
             else
             {
 
-               TempData["LoginError"] = "The username or password are incorrect";
+               TempData["LoginError"] = "The username and / or password are incorrect, please check again";
                 return RedirectToAction("AvailableItems", "Home");//??maybe to current page!
-
+                
+                
                 //need to show error message of "the user name or password are incorrect"
             }
         }
